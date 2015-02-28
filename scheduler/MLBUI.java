591a4d57651ced78c6bc048d582a185d16f23700
aimers1975 <amy_hindman@yahoo.com>
2015-02-27 22:53:17 -0600

    Created app to input and run an als file.

--- scheduler/MLBUI.java
+++ scheduler/MLBUI.java
@@ -1,46 +1,114 @@
+package scheduler;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JPanel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JScrollPane;
 import java.awt.EventQueue;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.GroupLayout;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import scheduler.MlbAppControl;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 //import statements
 //Check if window closes automatically. Otherwise add suitable code
 public class MLBUI extends JFrame {
 
+    MlbAppControl analyzer;
+    Boolean debug = true;
+    JLabel appInfo = new JLabel("Set Team Scheduling Parameters");
+    JButton quitButton = new JButton("Quit");
+    JButton saveModelButton = new JButton("Save Model");
+    JButton evaluateButton = new JButton("Evaluate");
+    JPanel scrollPanel = new JPanel();
+    JTextArea inputALS = new JTextArea("Type Alloy Code Here.",200,200);
+    JScrollPane scroll = new JScrollPane(inputALS);
+
 	public MLBUI() {
 		initUI();
+        analyzer = new MlbAppControl();
 	}
+
+    private void debug(String msg) {
+        if(debug) {
+            System.out.println(this.getClass().getSimpleName() + ": " + msg);
+        }
+    }
 
 	private void initUI() {
 		setTitle("Team Scheduler");
-        setSize(500, 500);
+        setSize(800, 500);
         setLocationRelativeTo(null);
-        JLabel appInfo = new JLabel("Set Team Scheduling Parameters");
-        JButton quitButton = new JButton("Quit");
-        JButton evaluateButton = new JButton("Evaluate");
-        JTextField inputALS = new JTextField("Input Alloy Code");
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
                 System.exit(0);
             }
         });
+        saveModelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                saveModelToFile();
+                debug("Model saved!");
+            }
+        });
         evaluateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                System.out.println("Evaluation started!");
+                try {
+                    analyzer.runAnalysis(getAnalyzerInput());
+                } catch(Exception e) {
+                    debug("Failure to run analyzer");
+                }
+                debug("Evaluation started!");
             }
         });
-        createLayout(appInfo, inputALS, evaluateButton, quitButton);
+
+        scrollPanel.setSize(230,230);
+        scrollPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        //scrollPanel.setBorder(new TitledBorder(new EtchedBorder(), "Display Area"));
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPanel.add(scroll);
+        
+        createLayout(appInfo, scroll, saveModelButton, evaluateButton, quitButton);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+
+    private void saveModelToFile() {
+
+        String[] lines = inputALS.getText().split("\\n");
+        try {
+            PrintWriter out = new PrintWriter("scheduler\\model.als");
+            for(String line : lines) {
+                debug(line);
+                out.println(line);
+            }
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Failed to create model file.");
+        }
+
+    }
+
+    private String[] getAnalyzerInput() {
+        String[] analyzerInputs = new String[1];
+        Path path = Paths.get("scheduler\\model.als");
+        Path absolutePath = path.toAbsolutePath();
+        analyzerInputs[0] = absolutePath.toString();
+        return analyzerInputs;
+    }
 
 	private void createLayout(JComponent... arg) {
 
@@ -55,6 +123,7 @@ public class MLBUI extends JFrame {
                 .addComponent(arg[1])
                 .addComponent(arg[2])
                 .addComponent(arg[3])
+                .addComponent(arg[4])
         );
 
         gl.setVerticalGroup(gl.createSequentialGroup()
@@ -62,6 +131,7 @@ public class MLBUI extends JFrame {
                 .addComponent(arg[1])
                 .addComponent(arg[2])
                 .addComponent(arg[3])
+                .addComponent(arg[4])
         );
     }
 
---
+++ scheduler/MlbAppControl.java
@@ -0,0 +1,73 @@
+package scheduler;
+
+import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.alloy4.ErrorWarning;
+import edu.mit.csail.sdg.alloy4compiler.ast.Command;
+import edu.mit.csail.sdg.alloy4compiler.ast.Module;
+import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
+import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
+import edu.mit.csail.sdg.alloy4viz.VizGUI;
+
+public class MlbAppControl {
+	
+    VizGUI viz = null;
+    A4Reporter rep;
+    Boolean debug = false;
+
+	public MlbAppControl() {
+	  rep = new A4Reporter() {
+	    // For example, here we choose to display each "warning" by printing it to System.out
+	    @Override public void warning(ErrorWarning msg) {
+	        debug("Relevance Warning:\n"+(msg.toString().trim())+"\n\n");
+	        System.out.flush();
+	    }};
+	}
+
+	public void debug(String msg) {
+		if(debug) {
+			System.out.println(this.getClass().getSimpleName() + ": " + msg);
+		}
+	}
+
+	public void runAnalysis(String[] inputfiles) throws Err {
+	        for(String filename:inputfiles) {
+
+            // Parse+typecheck the model
+            debug("=========== Parsing+Typechecking "+filename+" =============");
+            debug("Creating module.");
+            Module world = CompUtil.parseEverything_fromFile(rep, null, filename);
+            debug("Creating options");
+            // Choose some default options for how you want to execute the commands
+            A4Options options = new A4Options();
+            debug("Creating solver");
+            options.solver = A4Options.SatSolver.SAT4J;
+            debug("looping through commands");
+            for (Command command: world.getAllCommands()) {
+                // Execute the command
+                debug("============ Command "+command+": ============");
+                A4Solution ans = TranslateAlloyToKodkod.execute_command(rep, world.getAllReachableSigs(), command, options);
+                // Print the outcome
+                System.out.println(ans);
+                // If satisfiable...
+                if (ans.satisfiable()) {
+                    // You can query "ans" to find out the values of each set or type.
+                    // This can be useful for debugging.
+                    //
+                    // You can also write the outcome to an XML file
+                    ans.writeXML("alloy_example_output.xml");
+                    //
+                    // You can then visualize the XML file by calling this:
+                    if (viz==null) {
+                        viz = new VizGUI(false, "alloy_example_output.xml", null);
+                    } else {
+                        viz.loadXML("alloy_example_output.xml", true);
+                    }
+                }
+            }
+        }
+
+	}
+}
